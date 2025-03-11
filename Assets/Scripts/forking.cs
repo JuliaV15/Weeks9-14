6d2dc6d3c3dd8c892b9e78657155c171f6b151f2
versaeve <versaeve@OA-S141-20>
2025-03-11 19:05:36 -0400

    added the images for the next coding gym 👀

---
+++ Assets/Scripts/forking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class forking : MonoBehaviour
+{
+    public RectTransform latina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void corn()
+    {
+        Debug.Log("( ͡° ͜ʖ ͡°)");
+        latina.localScale = Vector3.one * 3f;
+    }
+
+    public void cob()
+    {
+        Debug.Log("🌽🌽🌽");
+        latina.localScale = Vector3.one;
+    }
+}

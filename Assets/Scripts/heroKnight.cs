07e82714f05b7c2e0f888fb3a7d26bf9fb3a42dd
versaeve <versaeve@OA-S141-20>
2025-03-25 16:31:02 -0400

    hero knight idle, run and attack animation that correlated with keyboard/mouse inputs

---
+++ Assets/Scripts/heroKnight.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroKnight : MonoBehaviour
+{
+    public float speed = 2;
+    Animator anim;
+    SpriteRenderer yumyum;
+    public bool canRun = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        yumyum = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal"); 
+        yumyum.flipX = direction < 0; // flips x axis when w or < button is pressed (hero knight turns left)
+
+        anim.SetFloat("speed", Mathf.Abs (direction)); // absolute || so that when we run backwards it still counts for the animation (no negatives) 
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("slash"); // triggers the slash in animation when mouse button clicked
+            canRun = false;
+        }
+
+        if (canRun == true) // makes so that guy can only move AFTER the attack animation has finished
+        {
+            transform.position += transform.right * direction * speed * Time.deltaTime; // makes the guy actually move
+        }
+    }
+
+    public void done() // public void for attack is done 
+    {
+        canRun = true;
+        Debug.Log("duck duck goose");
+    }
+}
